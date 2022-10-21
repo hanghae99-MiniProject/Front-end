643d74bcd96b011ea1e1b98641bf854cf1381690
Kim Minjoo <roses16.dev@gmail.com>
2022-10-22 01:43:09 +0900

    feature: Rank → Movie Info Component로 변경

---
+++ src/components/movieinfo/MovieInfo.jsx
@@ -0,0 +1,24 @@
+import { DivMovieInfoSection } from './style'
+
+export default function MovieInfo({ movieInfo, isSmall=false }){
+
+  const PERFECT_STAR = 5;
+  const space = isSmall ? 0 : 20;
+
+  return <DivMovieInfoSection isSmall={isSmall} star={(movieInfo.star / PERFECT_STAR) * 100}>
+    <img src={movieInfo.posterUrl} />
+  
+    <div className='moveInfoWrap'>
+      <h2>{movieInfo.title}</h2>
+      <hr />
+      <span className='star'>★★★★★ <span>★★★★★</span> </span> <span>|</span>
+      <span>{movieInfo.genre}</span> <span>|</span>
+      <span>{movieInfo.releaseDate}</span>
+      <hr />
+      <div style={{height:space}}></div>
+      <h3>Cast</h3>
+      {movieInfo.cast.map((actorName) => <span>{actorName}</span>)}
+    </div>
+  
+  </DivMovieInfoSection>
+}
--- src/components/movieinfo/style.js
+++ src/components/movieinfo/style.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-export const DivRankSection = styled.div`
+export const DivMovieInfoSection = styled.div`
   min-width: 454px;
   min-height: 250px;
-  max-width: 50vw;
-
+  height: ${props => props.isSmall? '250px' : '70vh'};
   margin: auto;
 
   background-color: rgba(0, 0, 0, 0.7);
@@ -14,27 +13,40 @@ export const DivRankSection = styled.div`
   flex-wrap: wrap;
   justify-content: center;
 
-
   img{
       min-height: 250px;
       max-height: 500px;
-      height: 50vh;
+      height: 100%;
       align-content: stretch;
+
+      border-radius: 15px;
     }
 
   .moveInfoWrap{
     min-width: 454px;
-    width: 30vw;
+    width: 30%;
     margin: 10px;
     color: #cccccc;
     
     span{
-      margin-left: 5px;
       margin-right: 15px;
     }
 
     hr{
       border-color: #999999;
+    }
+
+    .star{
+      color: #999999;
+      position: relative;
+
+      span{
+        position: absolute;
+        width: ${props => props.star}%;
+        left: 0px;
+        color: red;
+        overflow: hidden;
+      }
     }
   }
 `
