1210b3d6258caf02cc37fcddc074790e096e3f47
Kim Minjoo <roses16.dev@gmail.com>
2022-10-22 08:58:49 +0900

    fix: movie info map method key 추가

--- src/components/movieinfo/MovieInfo.jsx
+++ src/components/movieinfo/MovieInfo.jsx
@@ -4,6 +4,7 @@ export default function MovieInfo({ movieInfo, isSmall=false }){
 
   const PERFECT_STAR = 5;
   const space = isSmall ? 0 : 20;
+  const keys = new Array(movieInfo.cast.length).fill(1).map((key, index) => key = index + 1)
 
   return <DivMovieInfoSection isSmall={isSmall} star={(movieInfo.star / PERFECT_STAR) * 100}>
     <img src={movieInfo.posterUrl} />
@@ -11,13 +12,13 @@ export default function MovieInfo({ movieInfo, isSmall=false }){
     <div className='moveInfoWrap'>
       <h2>{movieInfo.title}</h2>
       <hr />
-      <span className='star'>★★★★★<span>★★★★★</span> </span> <span>|</span>
+      <span className='star'>★★★★★<span>★★★★★</span></span> <span>|</span>
       <span>{movieInfo.genre}</span> <span>|</span>
       <span>{movieInfo.releaseDate}</span>
       <hr />
       <div style={{height:space}}></div>
       <h3>Cast</h3>
-      {movieInfo.cast.map((actorName) => <span>{actorName}</span>)}
+      {movieInfo.cast.map((actorName, index) => <span key={keys[index]}>{actorName}</span>)}
     </div>
   
   </DivMovieInfoSection>
