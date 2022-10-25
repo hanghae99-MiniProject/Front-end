edf25a72f5d7896d021ac667b3a74d56fbddb8ca
Kim Minjoo <roses16.dev@gmail.com>
2022-10-25 20:56:34 +0900

    fix: review ID 입력

--- src/components/reviewlist/ReviewList.jsx
+++ src/components/reviewlist/ReviewList.jsx
@@ -2,8 +2,9 @@ import ReviewThumb from "../reviewthumb/ReviewThumb"
 import { DivReviewListBox } from './style'
 
 export default function ReviewList( { width, reviews } ){
+  console.log(reviews)
   return <DivReviewListBox width={width}>
-            {reviews.map(review => <ReviewThumb review={review} key={review.id}/>)}
+            {reviews.map(review => <ReviewThumb review={review} key={review.reviewId}/>)}
         </DivReviewListBox>
 
 }
