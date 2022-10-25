ac9ad790f736227b75fce1f0737a1506706c136c
Kim Minjoo <roses16.dev@gmail.com>
2022-10-25 20:56:20 +0900

    feat: Main BE API 연결

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,20 +1,28 @@
 import axios from "axios"
 import { useState, useEffect } from 'react'
 import ReviewInfo from "../components/reviewinfo/ReviewInfo"
+import { useCookies } from "react-cookie";
 
 export default function Main(){
-  const TEST_ID = '4';
+  // const TEST_ID = '4';
+  const [ cookie, setCookie, removeCookie ] = useCookies();
   const [ review, setReview ] = useState();
   const [ isLoading, setIsLoading ] = useState(true);
 
   useEffect(() => {
-    axios.get(`https://week3-board.herokuapp.com/review/${TEST_ID}`)
+    axios.get('http://43.201.55.251:8080/api/top-heart')
     .then(res => {
       setReview(res.data)
-      setIsLoading(false)
-      console.log('then')
-    })
-    console.log('useEffect')
+      axios.defaults.headers.get['authorization'] = cookie.token;
+      axios.defaults.headers.get['refresh-token'] = cookie.refreshtoken;
+      axios.get(`http://43.201.55.251:8080/api/reviews/${res.data}`)    
+      .then(res => {
+        if(res.data.success){
+          setReview(res.data.data)
+          setIsLoading(false)
+        }
+      })      
+    })    
   }, [])
   
   if(isLoading){
@@ -23,7 +31,5 @@ export default function Main(){
   } else {
     {console.log('render-false')}
     return <ReviewInfo review={review}/>
-    
   }
-  
 }
