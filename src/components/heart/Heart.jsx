016e793a090e16224325070f2a18326b9ad8f607
Kim Minjoo <roses16.dev@gmail.com>
2022-10-26 21:30:53 +0900

    feat: Heart action update

--- src/components/heart/Heart.jsx
+++ src/components/heart/Heart.jsx
@@ -1,17 +1,48 @@
 import emptyHeart from '../../image/heart-regular.svg'
 import fullHeart from '../../image/heart-solid.svg'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { DivHeart } from './style';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { API_URL } from '../../shared/Request';
 
-export default function Heart(){
-  const [liked, setLiked] = useState(false);
+export default function Heart({isLiked, reviewId, heartNum}){
+  const [ liked, setLike ] = useState(isLiked);
+  const [ heartCount, setHeartCount ] = useState(heartNum)
+  const [ cookie, setCookie, removeCookie ] = useCookies();
 
   const onClickHeart = () => {
-    console.log('Heart Click')
-    setLiked(!liked)
+    // 로그아웃 상태일 때 alert
+
+    axios.defaults.headers.post['authorization'] = cookie.token;
+    axios.defaults.headers.post['refresh-token'] = cookie.refreshtoken;
+    axios.post(`${API_URL}/api/review/${reviewId}/heart`)
+    .then(res => {
+      if(res.data){
+        if(res.data.data){
+          // Heart 누름
+          setLike(true)
+          setHeartCount(heartNum + 1)
+        } else {
+          //Heart 취소
+          setLike(false)
+          setHeartCount(heartNum)
+        }
+      } else {
+        alert('좋아요에 실패했습니다.')
+        window.location.reload();
+      }
+
+      console.log(res.data)
+      // setHeartCount(heartCount++)
+    })
   }
+  
+  useEffect(() => {
 
+  }, [liked])
 
-  return <div>
-    <img src={liked? fullHeart : emptyHeart} onClick={onClickHeart}style={{width:15}}/>
-  </div>
+  return <DivHeart>
+    <img src={liked? fullHeart : emptyHeart} onClick={onClickHeart}/> <div>{heartCount}</div>
+  </DivHeart>
 }
---
+++ src/components/heart/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+export const DivHeart = styled.div`
+  height: 15px;
+  display: inline-flex;
+  justify-content: center;
+  margin-left: 5px;
+  align-items: flex-end;
+  
+  img{
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    margin-top: 2px;
+  }
+div{
+    margin-left: 5px;
+    vertical-align: bottom;
+    line-height: 15px;
+
+  }
+`
