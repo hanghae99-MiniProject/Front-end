e265253340c1c3c96a20a976f61fac6a537e9087
Kim Minjoo <roses16.dev@gmail.com>
2022-10-26 22:01:17 +0900

    style: 글쓰기버튼 위치 변경

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import ReviewList from "../components/reviewlist/ReviewList"
 import Loading from "../components/loading/Loading";
+import ButtonWrite from "../components/buttonwrite/ButtonWrite"
 
 import { useCookies } from 'react-cookie';
 
@@ -23,7 +24,10 @@ export default function Review(){
   if(error)
     return <h1>Error</h1>
 
-  return <ReviewList width='100%' reviews={searchMovies.data} />
+  return <>
+        <ReviewList width='100%' reviews={searchMovies.data} />
+        <ButtonWrite />
+      </>
 
 
 }
