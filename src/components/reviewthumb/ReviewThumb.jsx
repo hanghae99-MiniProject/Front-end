f2252820f62d27855554cac53d0fcfb4811a1277
Kim Minjoo <roses16.dev@gmail.com>
2022-10-27 01:05:31 +0900

    fix: Review List에서 하트 갯수, 코멘트 갯수 추가

--- src/components/reviewthumb/ReviewThumb.jsx
+++ src/components/reviewthumb/ReviewThumb.jsx
@@ -2,7 +2,6 @@ import { DivThumbBox } from './style'
 import commentIcon from '../../image/comment.svg'
 
 export default function ReviewThumb( { review } ){
-  const commentCount = review.comments.length
   return <DivThumbBox>
         <a href={`/detail/${review.reviewId}`}>
           <div className='posterWrap'>
@@ -11,7 +10,7 @@ export default function ReviewThumb( { review } ){
         </a>
         <div className='infoWrap'>
           <p>{ review.reviewTitle }</p>
-          <p align="center"><span>❤ { review.heartNum || 0 }</span><span><img src={commentIcon} /> { commentCount }</span></p>
+          <p align="center"><span>❤ { review.heartNum || 0 }</span><span><img src={commentIcon} /> { review.commentNum }</span></p>
         </div>
       </DivThumbBox>
 }
